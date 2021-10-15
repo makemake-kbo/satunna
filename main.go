7eea354e04a6edf33a0121ca282d2c52eff3546f
Vukasin Gostovic <vukasin@gostovic.me>
2021-10-15 17:29:29 +0200

    remove deprecated comments

--- main.go
+++ main.go
@@ -10,9 +10,6 @@ import (
 
 func main() {
 
-	/* currently used to get input data to derive the seed.
-	 * change later to accomodate user input and any other args.
-	 */
 	inputData := os.Args[1];
 
 	file, err := os.Open(inputData);
